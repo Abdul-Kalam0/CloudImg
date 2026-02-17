25019c8cd3188b46e420272cb1f0b434eed4a2ce
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-02-17 12:19:13 +0530

    Designed UI fro CloudImg Project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,6 +52,7 @@ function App() {
       console.log(response);
       setUploadedImageUrl(response.data.data.imageUrl);
       setMessage("Image Uploaded successfully.");
+      setImage(null);
       getImages();
     } catch (error) {
       setMessage("Image Upload failed.");
