8253fe613cfcb45bb386db9422b4692acc0c4e44
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-03-12 10:02:53 +0530

    Updated rateLimiter setting

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -2,6 +2,6 @@ import rateLimit from "express-rate-limit";
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 mins
-  max: 5, // 5 requests per IP
+  max: 15, // 5 requests per IP
   message: "Too many attempts. Try again later.",
 });
