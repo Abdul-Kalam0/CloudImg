cd22a4e30976607d319dca8f49d44f9398d976b2
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-03-15 19:58:06 +0530

    Updated the settings of react-toastify

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -47,7 +47,7 @@ createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />,{/* ✅ Toast container (ONLY ONCE) */}
     <ToastContainer
       position="top-right"
-      autoClose={3000}
+      autoClose={2000}
       hideProgressBar={false}
       newestOnTop
       closeOnClick
