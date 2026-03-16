226ae13b239117b3a71012556c2c459c8d69126a
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-03-16 11:35:55 +0530

    Updated settings of clearCookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -212,7 +212,7 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
     });
     return res.status(200).json({
