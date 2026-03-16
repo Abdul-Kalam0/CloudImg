9f71f486f77e10187f9180cd650e70f06c310d58
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-03-16 11:28:32 +0530

    Updated Protected Route

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import api from "../services/api";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 export const ProtectedRoute = () => {
   const [loading, setLoading] = useState(true);
   const [isAuth, setIsAuth] = useState(false);
+
+  const location = useLocation();
 
   useEffect(() => {
     const getMe = async () => {
@@ -18,20 +19,21 @@ export const ProtectedRoute = () => {
         setLoading(false);
       }
     };
-    getMe();
-  }, []);
 
-  if (loading)
+    getMe();
+  }, [location.pathname]); // runs whenever route changes
+
+  if (loading) {
     return (
       <div className="flex justify-center items-center h-[60vh]">
         <div className="w-10 h-10 border-4 border-purple-500 border-t-transparent rounded-full animate-spin"></div>
       </div>
     );
-  if (!isAuth) return <Navigate to="/" />;
+  }
 
-  return (
-    <div>
-      <Outlet />
-    </div>
-  );
+  if (!isAuth) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
 };
